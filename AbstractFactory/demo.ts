599b7160abd62f81a6243d9ae522a874b5ec4168
Guoxiin <guoxiin@163.com>
2020-08-04 22:29:01 +0800

    fix:function should return string

--- AbstractFactory/demo.ts
+++ AbstractFactory/demo.ts
@@ -1,52 +1,52 @@
 interface AbstractFactory {
-    createProductA(): AbstractProductA;
-    createProductB(): AbstractProductB;
+  createProductA(): AbstractProductA;
+  createProductB(): AbstractProductB;
 }
 
 class ConcreteFactory1 implements AbstractFactory {
-    public createProductA(): AbstractProductA {
-        return new ConcreteProductA1();
-    }
-    public createProductB(): AbstractProductB {
-        return new ConcreteProductB1();
-    }
+  public createProductA(): AbstractProductA {
+    return new ConcreteProductA1();
+  }
+  public createProductB(): AbstractProductB {
+    return new ConcreteProductB1();
+  }
 }
 
 class ConcreteFactory2 implements AbstractFactory {
-    public createProductA(): AbstractProductA {
-        return new ConcreteProductA1();
-    }
-    public createProductB(): AbstractProductB {
-        return new ConcreteProductB1();
-    }
+  public createProductA(): AbstractProductA {
+    return new ConcreteProductA1();
+  }
+  public createProductB(): AbstractProductB {
+    return new ConcreteProductB1();
+  }
 }
 
 interface AbstractProductA {
-    usefulFunctionA(): string;
+  usefulFunctionA(): string;
 }
 
 class ConcreteProductA1 implements AbstractProductA {
-    public usefulFunctionA(): string {
-        return 'The result of the product A1.'
-    }
+  public usefulFunctionA(): string {
+    return "The result of the product A1.";
+  }
 }
 
 class ConcreteProductA2 implements AbstractProductA {
-    public usefulFunctionA(): string {
-        return 'The result of the product A2.'
-    }
+  public usefulFunctionA(): string {
+    return "The result of the product A2.";
+  }
 }
 
 interface AbstractProductB {
-    usefulFunctionB(): string;
-    anotherUsefulFunctionB(collaborator: AbstractProductA): string;
+  usefulFunctionB(): string;
+  anotherUsefulFunctionB(collaborator: AbstractProductA): string;
 }
 
 class ConcreteProductB1 implements AbstractProductB {
-    public usefulFunctionB(): string {
-
-    }
-    public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
-
-    }
+  public usefulFunctionB(): string {
+    return "";
+  }
+  public anotherUsefulFunctionB(collaborator: AbstractProductA): string {
+    return "";
+  }
 }
